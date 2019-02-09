c47f379e3e7e9b6f673d7b6a0599ce7a46d6452f
vil-c <amn9955@gmail.com>
2019-02-09 12:25:53 +0530

    removed warnings

--- app/src/main/java/com/example/restaurant/DishActivity.java
+++ app/src/main/java/com/example/restaurant/DishActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.GridLayout;
 import android.widget.RadioButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
